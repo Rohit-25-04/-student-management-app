b928a5dab011b4f30d16c8aaba1453349c71de24
Rohit Rawat <rohiirwt08@gmail.com>
2026-02-13 11:32:04 +0530

    fix cors and session cookie for cross origin login

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ import Courses from "./Models/Course.js";
 import transporter from "./Models/Mailer.js";
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5174",
   credentials:true,
 }));
 app.use(express.json());
